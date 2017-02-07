97b3098de9296f91e400e7190619c60e3834ab83
Chris Grimes <cjg1981@live.com>
2017-02-07 00:00:03 -0600

    Remove flow

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 npm run lint \
-&& npm run flow \
 && npm run lines \
 && npm run cover \
 && npm run bundle \
--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 npm run lint \
-&& npm run flow \
 && npm run test \
 && npm run bundle;
