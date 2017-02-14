bd48a2851256cf686c283e703c3040992ae9384e
Chris Grimes <cjg1981@live.com>
2017-02-14 16:33:05 -0600

    Fix a few errors

--- scripts/lines.sh
+++ scripts/lines.sh
@@ -1,8 +1,4 @@
-find . -name '*.js' \
-  | grep -v node_modules \
-  | grep -v bundle.js \
-  | grep -v coverage \
-  | xargs wc -l \
+find . -name '*.js' | grep -v node_modules | grep -v bundle | grep -v coverage | xargs wc -l \
 && echo \
-&& find . -name '*.scss' | grep -v node_modules | grep -v bundle.css | xargs wc -l \
+&& find . -name '*.scss' | grep -v node_modules | grep -v bundle | xargs wc -l \
 && echo;
